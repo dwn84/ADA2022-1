b9ca4f342b2b7944163d775c36c9d766d2899944
dwn84 <50523550+dwn84@users.noreply.github.com>
2022-02-10 09:10:00 -0500

    Update recursividad.py

--- recursividad.py
+++ recursividad.py
@@ -1,3 +1,5 @@
+import time
+
 #Generar recursivamente la potencia de dos números
 #a^b
 
@@ -21,5 +23,50 @@ def buscarNodo(a):
     
 def buscarNodoRec(raiz,a):
     buscarNodoRec(raiz+siguiente,a):
+    
+#Análisis fibonacci
+
+#Comportamiento no lineal
+def fibo1(n):
+    #caso base
+    if(n<2):
+        return n
+    else:
+        return fibo1(n-1) + fibo1(n-2)
+
+#Optimización con Memoización
+def fibo2(n,datos):
+    #caso base
+    if(n<2):
+        return n
+    elif n in datos:
+        return datos[n]
+    else:
+        datos[n] = fibo2(n-1,datos) + fibo2(n-2,datos)
+        return datos[n]
+
+#Perspectiva cíclica
+def fibo3(n, a, b):
+#caso base
+    if(n<2):
+        return a
+    else:
+        return fibo3(n-1,b,a+b)
+
+#Medición de tiempo de cada método Fibo
+
+tiempoInicial=time.time()
+
+diccionario = {}
+
+#print(fibo1(40))
+#print(fibo2(40,diccionario))
+print(fibo3(41,0,1))
+
+tiempoFinal=time.time()
+
+tiempoTotal=tiempoFinal - tiempoInicial
+
+print("Tiempo de ejecución total:{}". format(tiempoTotal))
     
 #recursividad mutua...
