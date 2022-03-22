8286cb31f8da4e7b327d4f932dcd6db5a5a21355
dwn84 <50523550+dwn84@users.noreply.github.com>
2022-03-22 09:21:49 -0500

    Create MetodosOrdenamiento.py

---
+++ MetodosOrdenamiento.py
@@ -0,0 +1,97 @@
+
+def WorstBubbleSort(listData,comparaciones):
+    
+    for i in range(len(listData)-1):
+         for j in range((len(listData)-1)):
+            if(listData[j]>listData[j+1]):                
+                listData[j],listData[j+1]= listData[j+1],listData[j]
+            comparaciones = comparaciones + 1                
+    print(comparaciones)
+    return listData
+
+def bubbleSort(listData,comparaciones):
+    
+    for i in range(len(listData)-1):
+         for j in range((len(listData)-1)-i):
+            if(listData[j]>listData[j+1]):                
+                listData[j],listData[j+1]= listData[j+1],listData[j]
+            comparaciones = comparaciones + 1                
+    print(comparaciones)
+    return listData
+
+
+def bubbleSortCheckOrdered(listData,comparaciones):
+    
+    for i in range(len(listData)-1):
+        sorted = True
+        for j in range((len(listData)-1)-i):
+           if(listData[j]>listData[j+1]):                
+               listData[j],listData[j+1]= listData[j+1],listData[j]
+               sorted = False
+           comparaciones = comparaciones + 1
+        if(sorted):
+            break
+               
+    print(comparaciones)
+    
+    return listData
+
+def recursiveBubbleSort(listData):
+    return recursiveBubbleSortI(listData,len(listData),0)
+
+def recursiveBubbleSortI(listData,n,contador):
+    if (n==1):
+        return listData
+
+    for j in range(n - 1):
+       if(listData[j]>listData[j+1]):                
+           listData[j],listData[j+1]= listData[j+1],listData[j]
+       contador = contador + 1   
+    print(contador)
+    return recursiveBubbleSortI(listData,n - 1, contador)
+
+def insertSort(listData):
+    comparaciones = 0
+    n = len(listData)
+    for i in range(1,n):
+        aux = listData[i]
+        j = i
+        while(j>0 and aux<listData[j-1]):
+            listData[j]=listData[j-1]
+            j = j - 1
+            comparaciones = comparaciones + 1
+        listData[j] = aux
+    print(f"Iteraciones método inserción:{comparaciones}")
+    return listData
+            
+def selectSort(listData):
+    comparaciones = 0
+    n = len(listData)
+    for i in range(0,n):
+        min = i
+        for j in range(i+1,n):
+            if(listData[j]<listData[min]):
+                min = j
+            comparaciones = comparaciones + 1
+        listData[i],listData[min]= listData[min],listData[i]
+        
+    print(f"Iteraciones método selección:{comparaciones}")
+    return listData    
+        
+        
+        
+#miLista = [1,2,3,4,5,6,7,8,9]#el mejor caso posible
+miLista = [11,10,9,8,7,6,5,4,3,2,1]#el peor caso posible
+miLista1 = miLista.copy()#el peor caso posible
+miLista2 = miLista.copy()#el peor caso posible
+miLista3 = miLista.copy()#el peor caso posible
+miLista4 = miLista.copy()#el peor caso posible
+milista5 = miLista.copy()
+
+print(WorstBubbleSort(miLista,0))
+print(bubbleSort(miLista1,0))
+print(bubbleSortCheckOrdered(miLista2,0))
+print(recursiveBubbleSort(miLista3))
+print(insertSort(miLista4))
+print(selectSort(miLista4))
+
